c07a29a331ca6c3bc2d15938562b54acec4efced
Roman Kowalski <roman.kowalski@dealercrm.pl>
2019-02-09 01:55:05 +0100

    bootstrap.js z node_modules

--- themes/aqua/includes/front/enqueue.php
+++ themes/aqua/includes/front/enqueue.php
@@ -6,39 +6,22 @@ function aqua_enqueue() {
 
 	$options = get_option('aqua_options');
 
-//	wp_register_style('aqua_bootstrap',
-//			get_template_directory_uri() . '/assets/styles/bootstrap.min.css');
-
 	wp_register_style('aqua_main', get_template_directory_uri() . '/assets/build/style.css', $ver='2.0');
-	
-//	wp_register_style('aqua_owl.carousel',
-//			get_template_directory_uri() . '/assets/styles/owl.carousel.min.css');
-	
-//	wp_register_style('aqua_owl.theme',
-//			get_template_directory_uri() . '/assets/styles/owl.theme.default.min.css');
-
-//	wp_register_style('aqua_lightbox',
-//			get_template_directory_uri() . '/assets/styles/simplelightbox.min.css');
-	
-	wp_register_style('aqua_fonts', 
+	wp_register_style('aqua_fonts',
 			'https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,regular,italic,600,600italic,700,700italic,800,800italic&subset=latin-ext');
 
 	wp_register_style('aqua_fonts-awesome', 
 			get_template_directory_uri() . '/assets/vendor/fontawesome/css/font-awesome.min.css');
 
-	wp_enqueue_style('aqua_bootstrap');
 	wp_enqueue_style('aqua_fonts');
 	wp_enqueue_style('aqua_main');
-	wp_enqueue_style('aqua_owl.carousel');
-	wp_enqueue_style('aqua_lightbox');
-	wp_enqueue_style('aqua_owl.theme');
 	wp_enqueue_style('aqua_fonts-awesome');
 
 	// SCRIPTS
 
 	wp_register_script('aqua_jquery', get_template_directory_uri() . '/assets/vendor/jquery-3.2.1.min.js', array(), false, true);
 	wp_register_script('aqua_owl', get_template_directory_uri() . '/node_modules/owl.carousel/dist/owl.carousel.min.js', array(), false, true);
-	wp_register_script('aqua_bootstrap', get_template_directory_uri() . '/assets/scripts/bootstrap.min.js', array(), false, true);
+	wp_register_script('aqua_bootstrap', get_template_directory_uri() . '/node_modules/bootstrap3/dist/js/bootstrap.min.js', array(), false, true);
 	wp_register_script('aqua_lightbox', get_template_directory_uri() . '/node_modules/simplelightbox/dist/simple-lightbox.min.js', array(), false, true);
 	wp_register_script('aqua_scroll', get_template_directory_uri() . '/assets/vendor/smooth-scroll.min.js', array(), false, true);
 	wp_register_script('aqua_imagesloaded', get_template_directory_uri() . '/assets/vendor/imagesloaded.pkgd.min.js', array(), false, true);
